a7f956fea47ca42f2f20bb4284c4ff2aed85a43a
Bruna Campos <92brunacampos@gmail.com>
2022-03-16 15:15:07 -0300

    primeiros passos em vue

---
+++ primeiro-projeto-vue/primeiro-projeto-vue/src/components/Contador.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<p>Contador:</p>
+		<div id="number">{{ number }}</div>
+		<button @click="add">+</button>
+		<button @click="sub">-</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Contador-exercise',
+	data() {
+		return {
+			number: 0
+		}
+	},
+	methods: {
+		add() {
+			this.number = this.number + 1;
+		},
+		sub() {
+			this.number = this.number - 1;
+		}
+	}
+}
+</script>
+
+// let add = document.getElementById('add');
+// let sub = document.getElementById('sub');
+// let number = document.getElementById('number');
+// let integer = 0;
+
+// add.addEventListener('click', function() {
+// 	integer += 1;
+// 	number.innerHTML = integer;
+
+// });
+
+// sub.addEventListener('click', function() {
+// 	integer -= 1;
+// 	number.innerHTML = integer;
+// });
---
+++ primeiro-projeto-vue/primeiro-projeto-vue/src/components/Eventos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="teste-evento">
+    <!-- <p>{{ mensagem }}</p> -->
+    <p v-if="valida" @click="deixarMinuscula">{{mensagem}}</p>
+    <p v-else @click="deixarMaiuscula">{{mensagem}}</p>
+  
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'TesteEvento',
+  data() {
+    return {
+      mensagem: 'O rato roeu a roupa do rei de Roma',
+			valida: true
+    }
+  },
+  methods: {
+    deixarMaiuscula() {
+      this.mensagem = this.mensagem.toUpperCase();
+			this.valida = false;
+    },
+		deixarMinuscula() {
+			this.mensagem = this.mensagem.toLowerCase();
+			this.valida = true;
+		}
+  }
+}
+</script>
